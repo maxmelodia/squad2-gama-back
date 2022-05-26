164b212ef953dc0b8d7292bec7da0e2ae9cd5f75
Maxwell Duarte <maxmelodia@gmail.com>
2022-05-25 23:06:12 -0300

    refactor: Ajuste na busca de conexoes

--- src/controllers/usuario/findAllConexao.js
+++ src/controllers/usuario/findAllConexao.js
@@ -1,24 +1,32 @@
 const db = require('../../models');
+const sequelize = require('sequelize');
 const ConexaoService = require('../../services/conexao');
 const HttpResponse = require('../../config/helpers/http-response');
 
 module.exports = {
   async findAllConexao(req, res) {
     try {
+          const { sub } = req.params;
           const conexaoService = new ConexaoService('Conexao', req);
 
-          const includes = {
+          const params = {
+            // where: {
+            //   sub: sequelize.col("usuario_publicou.sub")
+            // },
             include: [
               { model: db.Usuario, as: "usuario_conectou",
                 attributes: ['id', 'sub', 'usuario', 'nome', 'email', 'data_nascimento', 'cpf', 'cidade', 'telefone', 'descricao','foto']
               },
               { model: db.Usuario, as: "usuario_publicou",
-                attributes: ['id', 'sub', 'usuario', 'nome', 'email', 'data_nascimento', 'cpf', 'cidade', 'telefone', 'descricao', 'foto']
+                attributes: ['id', 'sub', 'usuario', 'nome', 'email', 'data_nascimento', 'cpf', 'cidade', 'telefone', 'descricao', 'foto'],
+                where: {
+                  sub: sub
+                }
               },
-            ]
+            ]            
           }
           
-          const data = await conexaoService.findAndCountAll({},includes);
+          const data = await conexaoService.findAll(params);
           return res.status(200).json(HttpResponse.ok(data))
     } catch (error) {
       return res.status(500).json(HttpResponse.serverError(error.message))
